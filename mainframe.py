f7bbb9cce6daf65c01b82f8561a65cea6d40e662
BrandonMontandon <62522685+BrandonMontandon@users.noreply.github.com>
2020-03-22 17:54:02 -0400

    Update mainframe.py

--- mainframe.py
+++ mainframe.py
@@ -5,9 +5,8 @@
 #Chris Magtibay
 #Max Zhang
 #Sean Sacchetti
-
+#Polynomial were using, f(x) = 1 + x + x^2 + x^ 5 + x^19
 ############  SSRG  ####################
-    #01000 = 1 + x^2 + x^5
     print("SSRG OUTPUT\n")
     reg1 = 1
     reg2 = 0
@@ -17,7 +16,6 @@
     for i in range(31):
         print("ITERATION #%d - reg1: %d   reg2: %d   reg3: %d   reg4: %d   reg5: %d"% (i, reg1, reg2, reg3, reg4, reg5)) 
         reg1, reg2, reg3, reg4, reg5 = (reg2+reg5)%2, reg1, reg2, reg3, reg4
-
 ############  MSRG  ####################
     print("\nMSRG OUTPUT\n")
     reg1 = 1
