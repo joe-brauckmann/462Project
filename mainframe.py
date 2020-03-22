8eb3c58248781466e418fab294ba98fb55291228
BrandonMontandon <62522685+BrandonMontandon@users.noreply.github.com>
2020-03-22 17:51:54 -0400

    Update mainframe.py

--- mainframe.py
+++ mainframe.py
@@ -5,3 +5,26 @@
 #Chris Magtibay
 #Max Zhang
 #Sean Sacchetti
+
+############  SSRG  ####################
+    #01000 = 1 + x^2 + x^5
+    print("SSRG OUTPUT\n")
+    reg1 = 1
+    reg2 = 0
+    reg3 = 0
+    reg4 = 0
+    reg5 = 0
+    for i in range(31):
+        print("ITERATION #%d - reg1: %d   reg2: %d   reg3: %d   reg4: %d   reg5: %d"% (i, reg1, reg2, reg3, reg4, reg5)) 
+        reg1, reg2, reg3, reg4, reg5 = (reg2+reg5)%2, reg1, reg2, reg3, reg4
+
+############  MSRG  ####################
+    print("\nMSRG OUTPUT\n")
+    reg1 = 1
+    reg2 = 0
+    reg3 = 0
+    reg4 = 0
+    reg5 = 0
+    for i in range(31):
+        print("ITERATION #%d - reg1: %d   reg2: %d   reg3: %d   reg4: %d   reg5: %d"% (i, reg1, reg2, reg3, reg4, reg5)) 
+        reg1, reg2, reg3, reg4, reg5 = reg5, reg1, (reg5+reg2)%2, reg3, reg4   
