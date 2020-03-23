6870d8080f71ad7090d70ca3f9cea30a6a6c47cc
Joseph Brauckmann <53092191+joe-brauckmann@users.noreply.github.com>
2020-03-23 14:31:48 -0400

    Update mainframe.py

--- mainframe.py
+++ mainframe.py
@@ -25,16 +25,18 @@ print(bitstream)
 
 
 #Polynomial were using, f(x) = 1 + x + x^2 + x^ 5 + x^19
+
 ############  SSRG  ####################
-    print("SSRG OUTPUT\n")
-    reg1 = 1
-    reg2 = 0
-    reg3 = 0
-    reg4 = 0
-    reg5 = 0
-    for i in range(31):
-        print("ITERATION #%d - reg1: %d   reg2: %d   reg3: %d   reg4: %d   reg5: %d"% (i, reg1, reg2, reg3, reg4, reg5)) 
-        reg1, reg2, reg3, reg4, reg5 = (reg2+reg5)%2, reg1, reg2, reg3, reg4
+#    print("SSRG OUTPUT\n")
+#    reg1 = 1
+#    reg2 = 0
+#    reg3 = 0
+#    reg4 = 0
+#    reg5 = 0
+#    for i in range(31):
+#        print("ITERATION #%d - reg1: %d   reg2: %d   reg3: %d   reg4: %d   reg5: %d"% (i, reg1, reg2, reg3, reg4, reg5)) 
+#        reg1, reg2, reg3, reg4, reg5 = (reg2+reg5)%2, reg1, reg2, reg3, reg4
+#^ commented out this code for right now because I don't think we need it, but uncomment it if I am wrong. -Joe	
 ############  MSRG  ####################
     print("\nMSRG OUTPUT\n")
     reg1 = 1
