1adf4d7b79b60c82477c9105742b2f3e86028dad
BrandonMontandon <62522685+BrandonMontandon@users.noreply.github.com>
2020-04-03 13:34:06 -0400

    Update mainframe2.py

--- mainframe2.py
+++ mainframe2.py
@@ -1,1 +1,30 @@
-Here it is!
+#Team Hac
+#Project 2
+#Brandon Bastian
+#Gavin Fisher
+#Joe Brauckman
+#Christopher Magtibay
+#Max Zhang
+#Sean Sacchetti
+
+
+#Python3
+import numpy as np
+import scipy.io
+import math
+
+################ Open the file & read it using SCIPY library ################
+#Rxstream = scipy.io.loadmat('ProjectRxSymbStream.mat')['RxSymbStream']
+RxTransposed = np.transpose(scipy.io.loadmat('ProjRxSymbStream.mat')['RxSymbStream']).tolist()
+
+
+################ Cyclic Prefix ################
+# Remove first 70 bits and store remaining 1024 into an array of lists
+# Each list is 1024 bytes 
+noPrefix = []
+for i in range(len(RxTransposed)):
+    noPrefix += RxTransposed[i][70:]
+
+
+################ Save output to matlab file ################
+scipy.io.savemat('RxOFDMSymb.mat',dict(OFDMSymbStream=np.array(noPrefix)),oned_as='row')
