75c56812dab5e9cbac9eaa3d0fcb47803e4a29e4
SeanSacchetti <46791074+SeanSacchetti@users.noreply.github.com>
2020-04-11 14:39:55 -0400

    Update mainframe3.py

--- mainframe3.py
+++ mainframe3.py
@@ -4,3 +4,11 @@
 import numpy as np
 import scipy.io
 import math
+
+OFDMSymb = []
+OFDMSymb = scipy.io.loadmat("RxOFDMSymb.mat")['OFDMSymbStream'][0].tolist()
+
+fftList = []
+fftList = np.fft.fft(OFDMSymb)
+
+scipy.io.savemat('FFTSymb.mat', dict(FFTSymb=np.array((fftList))), do_compression=True, oned_as='row')
