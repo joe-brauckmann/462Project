c8322190e894c040154f3980d2b9de8874854ee0
BrandonMontandon <62522685+BrandonMontandon@users.noreply.github.com>
2020-04-13 13:39:47 -0400

    Update mainframe3.py

--- mainframe3.py
+++ mainframe3.py
@@ -1,18 +1,31 @@
 #Project Part 3
 #Python3
 #Team Hac
+#Brandon Bastian
+#Gavin Fisher
+#Sean Sacchetti
+#Joe Brauckmann
+#Chris Magtibay
+#Max Zhang
+
+#################### Import Numpy and Scipy libraries for matlab file reading/saving ###########################
 import numpy as np
 import scipy.io
 import math
 
+#################### Open IFFT OFDM Symbol Stream ######################
 OFDMSymb = []
-OFDMSymb = scipy.io.loadmat("RxOFDMSymb.mat")['OFDMSymbStream'][0].tolist()
+OFDMSymb = scipy.io.loadmat("ProjectIFFTOfdmSymbolStream.mat")['OFDMSymbStream'][0].tolist()
 
+#################### Use FFT function ######################
 fftList = []
 for i in range(math.ceil(len(OFDMSymb)/1024)):
     temp = OFDMSymb[1024*i:1024*i+1024]
-    temp = np.fft.fft(temp)
+    temp = np.fft.fftn(temp)
     fftList.append(temp)
 
-np.reshape(fftList, -1)
+################### Create 2-D array for output ########################
+fftList = np.reshape(fftList, (1024,10000))
+
+################### Save 2-D array to matlab file ######################
 scipy.io.savemat('FFTSymb.mat', dict(FFTSymb=np.array((fftList))), do_compression=True, oned_as='row')
