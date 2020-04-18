79499821c7013dec9fc604b12bf586944d847311
BrandonMontandon <62522685+BrandonMontandon@users.noreply.github.com>
2020-04-18 15:04:27 -0400

    Update mainframe4.py

--- mainframe4.py
+++ mainframe4.py
@@ -1,1 +1,15 @@
+#Project Part 3
+#Python3
+#Team Hac
+#Brandon Bastian
+#Gavin Fisher
+#Sean Sacchetti
+#Joe Brauckmann
+#Chris Magtibay
+#Max Zhang
+
+#################### Import Numpy and Scipy libraries for matlab file reading/saving ###########################
+import numpy as np
+import scipy.io
+import math
 
