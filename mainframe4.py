1173782d65822b3f07f4a9b3587b3c2c2f9a5d66
BrandonMontandon <62522685+BrandonMontandon@users.noreply.github.com>
2020-04-18 22:22:01 -0400

    Update mainframe4.py

--- mainframe4.py
+++ mainframe4.py
@@ -1,4 +1,4 @@
-#Project Part 3
+#Project Part 4
 #Python3
 #Team Hac
 #Brandon Bastian
@@ -13,3 +13,35 @@ import numpy as np
 import scipy.io
 import math
 
+INarray = []
+INarray = scipy.io.loadmat("FFTSymb.mat")['FFTSymb'].tolist()
+
+INarray = np.reshape(INarray, -1)
+output = []
+
+for i in range(len(INarray)):
+    if (np.real(INarray[i]) < 0):
+        if(np.imag(INarray[i]) < 0):
+            x = 1
+            y = 1
+            output.append(x)
+            output.append(y)
+        else:
+            x = 1
+            y = 0
+            output.append(x)
+            output.append(y)
+    elif (np.real(INarray[i]) > 0):
+        if(np.imag(INarray[i]) < 0):
+            x = 0
+            y = 1
+            output.append(x)
+            output.append(y)
+        else:
+            x = 0
+            y = 0
+            output.append(x)
+            output.append(y)
+
+
+scipy.io.savemat('EncryptedData.mat', dict(EncryptedData=np.array(output)), do_compression=True, oned_as='row')
