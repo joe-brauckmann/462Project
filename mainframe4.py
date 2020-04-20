de124937312cc6f66c9436a7451b28bc51c08a4e
Joseph Brauckmann <53092191+joe-brauckmann@users.noreply.github.com>
2020-04-19 23:43:38 -0400

    Update mainframe4.py

--- mainframe4.py
+++ mainframe4.py
@@ -45,3 +45,37 @@ for i in range(len(INarray)):
 
 
 scipy.io.savemat('EncryptedData.mat', dict(EncryptedData=np.array(output)), do_compression=True, oned_as='row')
+
+
+PnSeq = []
+Bitstream = []
+PnSeq = scipy.io.loadmat("EncryptedData.mat")['EncryptedData'].tolist()
+
+reg1 = 1
+reg2 = 0
+reg3 = 0
+reg4 = 0
+reg5 = 0
+reg6 = 0
+reg7 = 0
+reg8 = 0
+reg9 = 0
+reg10 = 0
+reg11 = 0
+reg12 = 0
+reg13 = 0
+reg14 = 0
+reg15 = 0
+reg16 = 0
+reg17 = 0
+reg18 = 0
+reg19 = 0
+
+for i in range(2**19-1):
+    PnSeq.append(reg19)
+    ##print("ITERATION #%d - reg1: %d   reg2: %d   reg3: %d   reg4: %d   reg5: %d  reg6: %d reg7: %d reg8: %d reg9: %d reg10: %d reg11: %d reg12: %d reg13: %d reg14: %d reg15: %d reg16: %d reg17: %d reg18: %d reg19: %d "% (i, reg1, reg2, reg3, reg4, reg5, reg6, reg7, reg8, reg9, reg10, reg11, reg12, reg13, reg14, reg15, reg16, reg17, reg18, reg19)) 
+    reg1,reg2,reg3,reg4,reg5,reg6,reg7,reg8,reg9,reg10,reg11,reg12,reg13, reg14,reg15,reg16,reg17,reg18,reg19  = reg19, (reg1+reg19)%2, (reg2+reg19)%2, reg3, reg4, (reg5+reg19)%2, reg6, reg7, reg8, reg9, reg10, reg11, reg12, reg13, reg14, reg15, reg16, reg17, reg18   
+    for i in range(len(EncryptedData)):
+    Bitstream.append(EncryptedData[i]^PnSeq[i%(2**19-1)])
+    
+    scipy.io.savemat('BitStream.mat', dict(Bitstream=np.array([float(x) for x in Bitsrean])), do_compression=True, oned_as='row')
