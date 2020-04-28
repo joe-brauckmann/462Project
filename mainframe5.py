675fe04b05b02877def56b5061762b4acfce3cf0
CAGMagtibay <41831788+CAGMagtibay@users.noreply.github.com>
2020-04-28 13:25:10 -0400

    Update mainframe5.py

--- mainframe5.py
+++ mainframe5.py
@@ -1,1 +1,14 @@
 # CMPEN 462 Project Part 5
+#Python3
+#Team Hac
+#Brandon Bastian
+#Gavin Fisher
+#Sean Sacchetti
+#Joe Brauckmann
+#Chris Magtibay
+#Max Zhang
+
+#################### Import Numpy and Scipy libraries for matlab file reading/saving ###########################
+import numpy as np
+import scipy.io
+import math
