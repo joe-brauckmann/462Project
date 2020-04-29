442bf122dc5a407c6655812bc27c6d053fd5c093
Joseph Brauckmann <53092191+joe-brauckmann@users.noreply.github.com>
2020-04-29 00:07:17 -0400

    Update mainframereceiver.py

--- mainframereceiver.py
+++ mainframereceiver.py
@@ -89,35 +89,18 @@ for i in range(len(INarray)):
             output.append(y)
 
 
-######################## Decrypting Sequence ######################
-#Polynomial were using, f(x) = 1 + x + x^2 + x^ 5 + x^19
-reg1 = 1
-reg2 = 0
-reg3 = 0
-reg4 = 0
-reg5 = 0
-reg6 = 0
-reg7 = 0
-reg8 = 0
-reg9 = 0
-reg10 = 0
-reg11 = 0
-reg12 = 0
-reg13 = 0
-reg14 = 0
-reg15 = 0
-reg16 = 0
-reg17 = 0
-reg18 = 0
-reg19 = 0
-
+#####################################  SSRG Encryption ############################################
+#Polynomial were using, f(x) = 1 + x^3 + x^31 
+Registers = [1] + [0 for i in range(20)]
 PnSeq = []
-Decrypt =[]
-for i in range(2**19-1):
-    PnSeq.append(reg19) 
-    reg1,reg2,reg3,reg4,reg5,reg6,reg7,reg8,reg9,reg10,reg11,reg12,reg13, reg14,reg15,reg16,reg17,reg18,reg19  = reg19, (reg1+reg19)%2, (reg2+reg19)%2, reg3, reg4, (reg5+reg19)%2, reg6, reg7, reg8, reg9, reg10, reg11, reg12, reg13, reg14, reg15, reg16, reg17, reg18   
-for i in range(len(output)):
-    Decrypt.append(output[i]^PnSeq[i%(2**19-1)])
+Encrypt =[]
+for i in range(Pn_seq_length):
+    PnSeq.append(Registers[0] ^ Registers[7])
+    tmpReg = Registers[1]^Registers[20]
+    Registers = [tmpReg] + Registers[:20]
 
-
-scipy.io.savemat('DecryptedData.mat', dict(DecryptedData=np.array([float(x) for x in Decrypt])), do_compression=True, oned_as='row')
+for i in range(len(bitstream)):
+    Encrypt.append(bitstream[i]^PnSeq[i%(Pn_seq_length)])
+    
+scipy.io.savemat('PnSeq.mat', dict(PnSeq=np.array([float(x) for x in PnSeq])), do_compression=True, oned_as='row')
+scipy.io.savemat('EncryptedData.mat', dict(Encrypt=np.array([float(x) for x in Encrypt])), do_compression=True, oned_as='row')
